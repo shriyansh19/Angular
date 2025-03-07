15378a680030c20b610a864e5bce85c6b73991d6
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-03-07 13:27:46 +0530

    [Shriyansh Kesharwani] Add . UC5 -  To display Hello from BridgeLabz with a user inputted name - Ensure the userName is

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone : true,
   imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -11,7 +12,9 @@ export class AppComponent {
   title = 'my-app';
   imgURL = "https://github.com/user-attachments/assets/eccf9b76-5291-493b-a9be-b0ef482bbe92"
   url = "https://www.bridgelabz.com";
-  username : String = "";
+  userName : string = "";
+  nameError : String = "";
+  
   ngOnInit() : void{
     this.title = "Hello From BridgeLabz";
   }
@@ -20,5 +23,16 @@ export class AppComponent {
     console.log("Save button is clicked", $event);
     window.open(this.url, "_blank");
   }
+
+  onInput(event: Event): void {
+    const regex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (regex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect";
+  }
 }
+
+
 
