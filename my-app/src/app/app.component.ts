9835b589fbda52b5516e2ea7fa74817b93328501
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-03-07 11:50:36 +0530

    [Shriyansh Kesharwani] Add . UC3 -  Ability To launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -9,9 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   imgURL = "https://github.com/user-attachments/assets/eccf9b76-5291-493b-a9be-b0ef482bbe92"
+  url = "https://www.bridgelabz.com";
 
   ngOnInit() : void{
     this.title = "Hello From BridgeLabz";
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked", $event);
+    window.open(this.url, "_blank");
   }
 }
 
